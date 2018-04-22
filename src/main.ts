4e545b41eb7d3493c0583c8f268fbd6462b8c24b
Julien Stuyck <stuyck@gmail.com>
2018-04-22 04:42:54 +0200

    Creating alias for environment and app for cleaner imports

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { RootModule } from './app/modules/root/root.module';
-import { environment } from './environments/environment';
 
+import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
